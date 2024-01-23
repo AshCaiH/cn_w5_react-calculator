9901ae2ad15429306b73bf8fe7da1f8178c7596a
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-01-23 16:39:02 +0000

    Finished basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,56 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 
 const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const commands = [".","+","-","*","/","=","C"];
+const displaySize = 12;
 
 function App() {
-  const [result, setResult] = useState("0");
+  const [display, setDisplay] = useState("0");
   const [working, setWorking] = useState(0);
+  const [nextCalc, setNextCalc] = useState("");
   const [numpad, setNumpad] = useState(nums);
 
   const inputNum = (num) => {
-    // If the result starts with a 0, replace it with the
+    // If the display starts with a 0, replace it with the
     // number the user has input.
-    if (result == 0) setResult(num);
+    if (display === "0") setDisplay(num.toString());
     // If we don't turn result into a string before adding
     // the next number, it will just output the sum.
-    else setResult(result.toString() + num);
+    else setDisplay(display.toString() + num);
+
+    // If the nextCalc hasn't been set, clear out the working
+    // variable and treat this as a new calculation.
+    if (!nextCalc) setWorking(0);
+  }
+
+  // Use this regularly to address floating point errors.
+  const round = (num) => {
+    let factor = Math.pow(10, displaySize);
+    return Math.round(num * factor) / factor;
+  }
+
+  const calculate = () => {
+    if (working == 0) setWorking(display);
+
+    switch (nextCalc) {
+      case "+":
+        setWorking(parseFloat(working) + parseFloat(display)); 
+        break;
+      case "-":
+        setWorking(parseFloat(working) - parseFloat(display));
+        break;
+      case "*":
+        setWorking(parseFloat(working) * parseFloat(display));
+        break;
+      case "/":
+        setWorking(parseFloat(working) / parseFloat(display));
+        break;
+      default:
+        break;
+    }
+
+    setDisplay("0");
   }
 
   const inputCommand = (e) => {
@@ -23,24 +58,21 @@ function App() {
 
     switch (command) {
       case "C": 
-        setResult("0");
+        setDisplay("0");
         setWorking(0);
+        setNextCalc("");
         break;
-      case "+":
-        setWorking(parseFloat(result) + parseFloat(working));
-        setResult("0");
+      case ".":
+        setDisplay(display + ".");
         break;
-      case "-": 
-        setResult("0");
+      case "=":
+        calculate();
+        setNextCalc("");
         break;
-      case "*": 
-        setResult("0");
-        break;
-      case "/": 
-        setResult("0");
-        break;
-      default: 
-        console.log(command)
+      default:
+        // User can change calc function after picking one.
+        if (display != 0) calculate();
+        setNextCalc(command);
     }
   }
 
@@ -48,8 +80,10 @@ function App() {
     <>
       <h1>Calculator</h1>
 
-      <p className="working">{working}</p>
-      <p className="result">{result}</p>
+
+      <p className="working">{parseFloat(round(working))}</p>
+      <p className="calcType">{nextCalc}</p>
+      <p className="display">{display}</p>
 
       <div className="numpad">
         {nums.map((num, index) => {
